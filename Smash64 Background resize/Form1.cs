a679752d7ecd675cfd4fa0801ccdf927d7dd7473
Jacob Bloomfield <blo15016@byui.edu>
2020-01-02 02:57:04 -0800

    clean up code, basic check for proper input

--- Smash64 Background resize/Form1.cs
+++ Smash64 Background resize/Form1.cs
@@ -25,12 +25,19 @@ namespace Smash64_Background_resize
 
         private void button2_Click(object sender, EventArgs e)
         {
+            /* img2 is a 300x220 copy of the input image
+             * img1 is 300x263, and will be the final image after rows are copied
+             */
             img1 = new Image<Bgr, byte>(filePath);
             img2 = new Image<Bgr, byte>(300, 220);
+
+            // if resize input width to 220
             if (img1.Height == 263 && img1.Width == 300)
             {
                 img2 = img1.Resize(300, 220, Emgu.CV.CvEnum.Inter.Nearest);
             }
+
+            // for 220 width input, make a 263 width copy
             else if (img1.Height == 220)
             {
                 Size size1 = new Size(300, 263);
@@ -38,11 +45,23 @@ namespace Smash64_Background_resize
                 img1 = img2.Resize(300, 263, Emgu.CV.CvEnum.Inter.Nearest);
             }
 
+            // basic size check 
+            else
+            {
+                DialogResult result;
+                result = MessageBox.Show("Invalid Image size, Supported sizes are 300x220 and 300x263", "Invalid Image", MessageBoxButtons.OK);
+                return;
+            }
+
+            // copy first 6 rows, row 7 is a duplicate
             for (int i = 0; i < 7; i++)
                 copyRow(i, i);
             copyRow(6, 7);
+
             int count = 0;
             int row = 0;
+
+            // from row 8, every 5th row is a duplicate
             for (int i = 8;i <221; i++)
             {
                 if (i + row > 263)
@@ -64,6 +83,7 @@ namespace Smash64_Background_resize
 
         }
 
+        // copies a row of pixels, one at a time from one image to another
         private void copyRow(int rowSource, int rowDest)
         {
             for (int i = 0; i < 300; i++)
@@ -72,6 +92,7 @@ namespace Smash64_Background_resize
             }
         }
 
+        // load and display file
         private void button1_Click(object sender, EventArgs e)
         {
             openFileDialog1 = new OpenFileDialog();
@@ -88,6 +109,7 @@ namespace Smash64_Background_resize
             }
         }
 
+        // open save dialog
         private void button3_Click(object sender, EventArgs e)
         {
             SaveFileDialog Dialog1 = new SaveFileDialog();
